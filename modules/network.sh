ba828543d98dfee05d0eae42785075593eb774af
Greedtik <168409428+Greedtik@users.noreply.github.com>
2026-03-16 16:12:02 +0700

    Improve network_scan function output
    Enhance network_scan function to display established connections with improved formatting.

--- modules/network.sh
+++ modules/network.sh
@@ -1,4 +1,4 @@
-network_scan(){
-echo "[Network Connections]"
-ss -plant
+network_scan() {
+    echo -e "\n\e[1;36m[+] Network Connections (Established)\e[0m"
+    ss -antup | grep ESTAB || echo -e "\e[1;32mNo established connections found.\e[0m"
 }
