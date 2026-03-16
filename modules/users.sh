73f2cf5c511efe126d2ad76f72024f5973287015
Greedtik <168409428+Greedtik@users.noreply.github.com>
2026-03-16 16:13:35 +0700

    Update users.sh

--- modules/users.sh
+++ modules/users.sh
@@ -1,4 +1,6 @@
-user_scan(){
-echo "[UID 0 Users]"
-awk -F: '$3==0 {print}' /etc/passwd
+user_scan() {
+    echo -e "\n\e[1;36m[+] UID 0 (Privileged) Users Scan\e[0m"
+    # ดึงเฉพาะ User ที่มี UID เป็น 0 (เทียบเท่า root)
+    local found=$(awk -F: '($3 == "0") {print}' /etc/passwd)
+    echo -e "\e[1;31m$found\e[0m"
 }
